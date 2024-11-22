bf9c96801c08b0e19b0d3bdb4cc9de340d7df3ab
Ubuntu <ubuntu@ip-172-31-5-98.ap-south-1.compute.internal>
2024-11-22 05:46:59 +0000

    Modified PrintReg.java

--- PrintReg.java
+++ PrintReg.java
@@ -1,5 +1,5 @@
 public class PrintReg {
     public static void main(String[] args) {
-        System.out.println("FA21-BCS-065");
+        System.out.println("Hello I'm running in Docker through Jenkins.\nFA21-BCS-065");
     }
 }
